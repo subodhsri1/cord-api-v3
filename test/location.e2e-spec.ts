7a92fdeecb668344656af96f854a625c0aa7fb6c
Michael Marshall <michaelmarshall220@gmail.com>
2020-01-15 22:03:02 -0600

    working on user login. refactored user service, resolver, class, and tests. mocking tokens for now. basic createUser is now schema correct.

--- test/location.e2e-spec.ts
+++ test/location.e2e-spec.ts
@@ -4,7 +4,6 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { isValid } from 'shortid';
 import { CreateLocationInput } from '../src/components/location/location.dto';
-import { DatabaseUtility } from '../src/common/database-utility';
 
 describe('Location e2e', () => {
   let app: INestApplication;
